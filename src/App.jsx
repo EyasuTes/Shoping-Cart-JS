7407c84f4240480896abeec89239cf9520a099c3
Eyasu Araya <128852829+EyasuTes@users.noreply.github.com>
2023-11-11 11:39:53 +0400

    added a error page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import { useState } from 'react'
-import {  Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Store from './pages/Store'
-import Cart from './pages/Cart'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import { Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
 function App() {
-
-
   return (
-    <div className=' relative ' >
-        <div  style={{ position: 'fixed', top: 0 , width:'100%', zIndex:'1'}}>
-          <Navbar ></Navbar>
-        </div>
-        
-        <Routes >
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/store' element={<Store/>}></Route>
-          
-          <Route path='/cart' element={<Cart/>}></Route>
-          
-        </Routes>
-     
+    <div className=" relative ">
+      <div style={{ position: "fixed", top: 0, width: "100%", zIndex: "1" }}>
+        <Navbar></Navbar>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/store" element={<Store />}></Route>
+
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
